59e979279322e5f8b7756d184feca60db9c0b720
Jeff Muizelaar <jrmuizel@gmail.com>
2019-07-20 21:47:32 -0400

    Add a new test that has cycles.
    Don't actually collect them yet because that's broken.

--- src/lib.rs
+++ src/lib.rs
@@ -1106,4 +1106,30 @@ mod tests {
         let x = Cc::new(map);
         assert_eq!(x.get("Foo"), Some(&4));
     }
+
+    #[test]
+    fn list_cycle() {
+        use std::cell::RefCell;
+
+        struct List(Vec<Cc<RefCell<List>>>);
+        impl Trace for List {
+            fn trace(&mut self, tracer: &mut Tracer) {
+                self.0.trace(tracer);
+            }
+        }
+        use collect::collect_cycles;
+        {
+            let a = Cc::new(RefCell::new(List(Vec::new())));
+            let b = Cc::new(RefCell::new(List(Vec::new())));
+            {
+                let mut a = a.borrow_mut();
+                a.0.push(b.clone());
+            }
+            {
+                let mut b = b.borrow_mut();
+                b.0.push(a.clone());
+            }
+        }
+        //collect_cycles();
+    }
 }
