99a7b1e87781e579d13f41b33d7a0f7477b73176
Jeff Muizelaar <jrmuizel@gmail.com>
2022-07-21 19:59:20 -0400

    Switch to as_ref() instead of as_mut()

--- src/cc_box_ptr.rs
+++ src/cc_box_ptr.rs
@@ -71,15 +71,15 @@ pub trait CcBoxPtr: Trace {
 }
 
 /// Deallocate the box if possible. `s` should already have been dropped.
-pub unsafe fn free(mut s: NonNull<dyn CcBoxPtr>) {
-    debug_assert!(s.as_mut().strong() == 0);
-    debug_assert!(!s.as_mut().buffered());
+pub unsafe fn free(s: NonNull<dyn CcBoxPtr>) {
+    debug_assert!(s.as_ref().strong() == 0);
+    debug_assert!(!s.as_ref().buffered());
 
     // Remove the implicit "strong weak" pointer now that we've destroyed
     // the contents.
-    s.as_mut().dec_weak();
+    s.as_ref().dec_weak();
 
-    if s.as_mut().weak() == 0 {
+    if s.as_ref().weak() == 0 {
         crate::deallocate(s);
     }
 }
